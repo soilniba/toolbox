85e7aa4627e734ad505fa48e2bda760e80d994c1
王畅 <wangchang@wangyuan.com>
2023-03-14 19:20:31 +0800

    小小优化

--- service/openai.js
+++ service/openai.js
@@ -30,11 +30,8 @@ router.all('/openai', async ({ query: { string, user } }, response) => {
   }
 
   // 获取该用户的聊天记录数组，如果不存在则新建一个空数组
-  const { messages = [] } = JSON.parse(localStorage[user] || '{}')
-  let new_question = true
-  if (localStorage[user]) {
-    new_question = false
-  }
+  const { messages = [] } = JSON.parse(localStorage[user] || '[]')
+  let new_question = !!localStorage[user];
 
   messages.push({ role: 'user', content: string })
   try {
@@ -52,7 +49,7 @@ router.all('/openai', async ({ query: { string, user } }, response) => {
     localStorage.setItem(user, JSON.stringify({ messages }))
 
     if (new_question) {
-      completion.data.choices[0].message.content = '🆕这是一个新问题的开始：\n(已闲置超过一小时或刚使用过/new指令)\n\n' + completion.data.choices[0].message.content
+      completion.data.choices[0].message.content = '🆕这是一个新问题的开始：(闲置超过一小时或使用过/new指令)\n\n' + completion.data.choices[0].message.content
     }
     response.send({
       choices: completion.data.choices
